8fbaecba66cae873951d783bf330cd8fdf8a85aa
zeroyouth <choi46910@naver.com>
2021-12-19 20:55:47 +0900

    wine 페이지 생성

---
+++ src/pages/wine.tsx
@@ -0,0 +1,12 @@
+import type {NextPage} from "next";
+
+const WinePage: NextPage = () => { //타입스크립트에서 타입선언
+    return (
+        <div>
+            <h1>Wine</h1>
+        </div>
+    )
+}
+
+export default WinePage;
+//가장 기본적인 템플릿임. TypeScript를 쓰는
